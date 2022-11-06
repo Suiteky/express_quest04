d3efa3d3f9f49d63e2ece09e4ea7e4c9064e563c
Julien GRELLIER <grllr.julian.ptrs@gmail.com>
2022-11-07 00:23:22 +0100

    quest03

--- userHandlers.js
+++ userHandlers.js
@@ -30,7 +30,25 @@ const getUserById = (req, res) => {
     });
 };
 
+const postUser = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
+  postUser,
 };
